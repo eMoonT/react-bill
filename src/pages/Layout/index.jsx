fe59ed14979e1901f023bb36bc9b1d58ca7a8886
eMoonT <1163060734@qq.com>
2023-12-16 18:04:03 +0800

    add Tabbar layout

--- src/pages/Layout/index.jsx
+++ src/pages/Layout/index.jsx
@@ -1,19 +1,56 @@
-import { useEffect } from "react"
-import { useDispatch } from "react-redux"
-import { Outlet } from "react-router-dom"
-import { fetchBillList } from "../../store/modules/billStore"
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Outlet } from "react-router-dom";
+import { fetchBillList } from "../../store/modules/billStore";
+import {
+  AddCircleOutline,
+  BillOutline,
+  CalculatorOutline,
+} from "antd-mobile-icons";
+import { TabBar } from "antd-mobile";
+import { useNavigate } from "react-router-dom";
 
 const Layout = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchBillList())
-  },[dispatch])
+    dispatch(fetchBillList());
+  }, [dispatch]);
+
+  const tabs = [
+    {
+      key: "/",
+      title: "月度账单",
+      icon: <BillOutline />,
+    },
+    {
+      key: "/new",
+      title: "记账",
+      icon: <AddCircleOutline />,
+    },
+    {
+      key: "/year",
+      title: "年度账单",
+      icon: <CalculatorOutline />,
+    },
+  ];
+
+  const navigate = useNavigate()
+  const switchChange = (path) => {
+    navigate(path)
+  }
 
   return (
-    <div>Layout
-      <Outlet />
+    <div className="w-full h-full">
+      <div className="w-full fixed bottom-[55px] inset-x-0">
+        <Outlet />
+      </div>
+      <TabBar onChange={(path) => switchChange(path)} className="w-full fixed bottom-0 inset-x-0">
+        {tabs.map((item) => (
+          <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+        ))}
+      </TabBar>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
